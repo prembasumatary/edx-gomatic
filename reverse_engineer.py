8340f5279f1db135a66ef2f8861ab66fe5830080
Brian Beggs <bbeggs@edx.org>
2016-06-15 17:06:46 -0400

    reverse engineer now works correctly

--- reverse_engineer.py
+++ reverse_engineer.py
@@ -6,16 +6,18 @@ def as_python(self, pipeline, with_save=True):
     head = "#!/usr/bin/env python\n\n" \
              "from gomatic import *\n\n" \
              "import click\n" \
-             "configurator = " + str(self) + "\n"
+             "import edxpipelines.utils as utils\n"
 
-    head = "@click.command()\n"\
+    head += "@click.command()\n"\
            "@click.option('--save-config', 'save_config_locally', envvar='SAVE_CONFIG', help='Save the pipeline configuration xml locally', required=False, default=False)\n"\
            "@click.option('--dry-run', envvar='DRY_RUN', help='do a dry run of  the pipeline installation, and save the pre/post xml configurations locally', required=False, default=True)\n"\
            "@click.option('--variable_file', 'variable_files', multiple=True, help='path to yaml variable file with a dictionary of key/value pairs to be used as variables in the script', required=False)\n"\
            "@click.option('-e', '--variable', 'cmd_line_vars', multiple=True, help='key/value of a variable used as a replacement in this script', required=False, type=(str, str), nargs=2)\n"\
-           "def install_pipeline(save_config_locally=False, dry_run=False, variable_files=[], cmd_line_vars={}):"
+           "def install_pipeline(save_config_locally=False, dry_run=False, variable_files=[], cmd_line_vars={}):\n"
 
-    result = "config = merge_files_and_dicts(variable_files, cmd_line_vars)\n\n"
+    result = "config = utils.merge_files_and_dicts(variable_files, cmd_line_vars)\n\n"
+    result += "configurator = " + str(self) + "\n"
+    result += "pipeline = configurator"
     result += pipeline.as_python_commands_applied_to_server()
     if with_save:
         result += "\n\nconfigurator.save_updated_config(save_config_locally=save_config_locally, dry_run=dry_run)"
