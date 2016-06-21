1a603cbc130ba1fa6f5878cc6f3d8074174bf40e
Brian Beggs <bbeggs@edx.org>
2016-06-21 14:40:59 -0400

    Formatting improvements from code review with doctoryes

--- edxpipelines/pipelines/build_edxapp_ami.py
+++ edxpipelines/pipelines/build_edxapp_ami.py
@@ -7,9 +7,9 @@ from gomatic import *
 @click.command()
 @click.option('--save-config', 'save_config_locally', envvar='SAVE_CONFIG', help='Save the pipeline configuration xml locally', required=False, default=False)
 @click.option('--dry-run', envvar='DRY_RUN', help='do a dry run of  the pipeline installation, and save the pre/post xml configurations locally', required=False, default=False)
-@click.option('--variable_file', 'variable_files', multiple=True, help='path to yaml variable file with a dictionary of key/value pairs to be used as variables in the script', required=False)
-@click.option('-e', '--variable', 'cmd_line_vars', multiple=True, help='key/value of a variable used as a replacement in this script', required=False, type=(str, str), nargs=2)
-def install_pipeline(save_config_locally, dry_run, variable_files=[], cmd_line_vars={}):
+@click.option('--variable_file', 'variable_files', multiple=True, help='path to yaml variable file with a dictionary of key/value pairs to be used as variables in the script', required=False, default=[])
+@click.option('-e', '--variable', 'cmd_line_vars', multiple=True, help='key/value of a variable used as a replacement in this script', required=False, type=(str, str), nargs=2, default={})
+def install_pipeline(save_config_locally, dry_run, variable_files, cmd_line_vars):
     """
     Variables needed for this pipeline:
     - gocd_username
@@ -86,111 +86,109 @@ def install_pipeline(save_config_locally, dry_run, variable_files=[], cmd_line_v
     job.add_task(ExecTask(['/bin/bash', '-c', 'sudo pip install -r requirements.txt'], working_dir="configuration"))
 
     # Setup configuration secure
-    job.add_task(ExecTask(['/bin/bash', '-c', "touch github_key.pem && "
-                                              "chmod 600 github_key.pem && "
-                                              'python tubular/scripts/format_rsa_key.py --key "$PRIVATE_GITHUB_KEY" --output-file github_key.pem && '
-                                              "GIT_SSH_COMMAND='/usr/bin/ssh -o StrictHostKeyChecking=no -i github_key.pem' /usr/bin/git clone $CONFIGURATION_SECURE_REPO secure_repo && "
-                                              "cd secure_repo && "
-                                              "/usr/bin/git checkout $CONFIGURATION_SECURE_VERSION && "
-                                              "mkdir ../target/ && "
-                                              "/usr/bin/git rev-parse HEAD > ../target/config_secure_sha"]
-                          )
-                 )
+    job.add_task(ExecTask(['/bin/bash',
+                           '-c',
+                           "touch github_key.pem && "
+                           "chmod 600 github_key.pem && "
+                           'python tubular/scripts/format_rsa_key.py --key "$PRIVATE_GITHUB_KEY" --output-file github_key.pem && '
+                           "GIT_SSH_COMMAND='/usr/bin/ssh -o StrictHostKeyChecking=no -i github_key.pem' /usr/bin/git clone $CONFIGURATION_SECURE_REPO secure_repo && "
+                           "cd secure_repo && "
+                           "/usr/bin/git checkout $CONFIGURATION_SECURE_VERSION && "
+                           "mkdir ../target/ && "
+                           "/usr/bin/git rev-parse HEAD > ../target/config_secure_sha"]))
 
     # Check out the requested version of configuration
     # This is a work around to add the ability to checkout a specific git sha, an option that gocd does not allow.
-    job.add_task(ExecTask(['/bin/bash', '-c', "/usr/bin/git fetch && "
-                                              "/usr/bin/git pull && "
-                                              "/usr/bin/git checkout $CONFIGURATION_VERSION"],
-                          working_dir="configuration/"
-                          )
-                 )
+    job.add_task(ExecTask(['/bin/bash',
+                           '-c', "/usr/bin/git fetch && "
+                           "/usr/bin/git pull && "
+                           "/usr/bin/git checkout $CONFIGURATION_VERSION"],
+                          working_dir="configuration/"))
 
     # Launch instance
-    job.add_task(ExecTask(['/bin/bash', '-c', 'ansible-playbook '
-                                              '-vvvv '
-                                              '--module-path=../../configuration/playbooks/library '
-                                              '-i "localhost," '
-                                              '-c local '
-                                              '-e artifact_path=`/bin/pwd`/../../../$ARTIFACT_PATH '
-                                              '-e ec2_subnet_id=$EC2_SUBNET_ID '
-                                              '-e base_ami_id=$BASE_AMI_ID '
-                                              '-e ec2_vpc_subnet_id=$EC2_VPC_SUBNET_ID '
-                                              '-e ec2_security_group_id=$EC2_SECURITY_GROUP_ID '
-                                              '-e ec2_instance_type=$EC2_INSTANCE_TYPE '
-                                              'launch_instance.yml'],
-                          working_dir="configuration/playbooks/continuous_delivery/"
-                          )
-                 )
+    job.add_task(ExecTask(['/bin/bash',
+                           '-c', 'ansible-playbook '
+                           '-vvvv '
+                           '--module-path=../../configuration/playbooks/library '
+                           '-i "localhost," '
+                           '-c local '
+                           '-e artifact_path=`/bin/pwd`/../../../$ARTIFACT_PATH '
+                           '-e ec2_subnet_id=$EC2_SUBNET_ID '
+                           '-e base_ami_id=$BASE_AMI_ID '
+                           '-e ec2_vpc_subnet_id=$EC2_VPC_SUBNET_ID '
+                           '-e ec2_security_group_id=$EC2_SECURITY_GROUP_ID '
+                           '-e ec2_instance_type=$EC2_INSTANCE_TYPE '
+                           'launch_instance.yml'],
+                          working_dir="configuration/playbooks/continuous_delivery/"))
 
     # run the edxapp play
-    job.add_task(ExecTask(['/bin/bash', '-c', 'export ANSIBLE_HOST_KEY_CHECKING=False;'
-                                              'export ANSIBLE_SSH_ARGS="-o ControlMaster=auto -o ControlPersist=30m";'
-                                              'PRIVATE_KEY=`/bin/pwd`/../../../${ARTIFACT_PATH}key.pem;'
-                                              'ansible-playbook '
-                                              '-vvvv '
-                                              '--private-key=$PRIVATE_KEY '
-                                              '--user=ubuntu '
-                                              '--module-path=configuration/playbooks/library '
-                                              '-i ../../../target/ansible_inventory '
-                                              '-e @../../../target/launch_info.yml '
-                                              '-e @../../../secure_repo/ansible/vars/${EDX_ENVIRONMENT}-${DEPLOYMENT}.yml '
-                                              '-e cache_id=$CACHE_ID '
-                                              '-e edx_platform_version=$CLUSTER_VERSION '
-                                              '-e edx_platform_repo=$CLUSTER_REPO '
-                                              '-e edxapp_theme_source_repo=$EDX_APP_THEME_REPO  '
-                                              '-e edxapp_theme_version=$EDX_APP_THEME_VERSION  '
-                                              '-e edxapp_theme_name=$EDXAPP_THEME_NAME  '
-                                              '../edx-east/edxapp.yml'],
-                          working_dir="configuration/playbooks/continuous_delivery/"
-                          )
-                 )
+    job.add_task(ExecTask(['/bin/bash',
+                           '-c',
+                           'export ANSIBLE_HOST_KEY_CHECKING=False;'
+                           'export ANSIBLE_SSH_ARGS="-o ControlMaster=auto -o ControlPersist=30m";'
+                           'PRIVATE_KEY=`/bin/pwd`/../../../${ARTIFACT_PATH}key.pem;'
+                           'ansible-playbook '
+                           '-vvvv '
+                           '--private-key=$PRIVATE_KEY '
+                           '--user=ubuntu '
+                           '--module-path=configuration/playbooks/library '
+                           '-i ../../../target/ansible_inventory '
+                           '-e @../../../target/launch_info.yml '
+                           '-e @../../../secure_repo/ansible/vars/${EDX_ENVIRONMENT}-${DEPLOYMENT}.yml '
+                           '-e cache_id=$CACHE_ID '
+                           '-e edx_platform_version=$CLUSTER_VERSION '
+                           '-e edx_platform_repo=$CLUSTER_REPO '
+                           '-e edxapp_theme_source_repo=$EDX_APP_THEME_REPO  '
+                           '-e edxapp_theme_version=$EDX_APP_THEME_VERSION  '
+                           '-e edxapp_theme_name=$EDXAPP_THEME_NAME  '
+                           '../edx-east/edxapp.yml'],
+                          working_dir="configuration/playbooks/continuous_delivery/"))
 
     # Create an AMI from the instance
-    job.add_task(ExecTask(['/bin/bash', '-c', 'ansible-playbook '
-                                              '-vvvv '
-                                              '--module-path=../../playbooks/library '
-                                              '-i "localhost," '
-                                              '-c local '
-                                              '-e @../../../target/launch_info.yml '
-                                              '-e play=$PLAY '
-                                              '-e deployment=$DEPLOYMENT '
-                                              '-e edx_environment=$EDX_ENVIRONMENT '
-                                              '-e cluster_repo=$CLUSTER_REPO '
-                                              '-e cluster_version=$CLUSTER_VERSION '
-                                              '-e configuration_repo=$CONFIGURATION_REPO '
-                                              '-e configuration_version=$CONFIGURATION_VERSION '
-                                              '-e configuration_secure_repo=$CONFIGURATION_SECURE_REPO '
-                                              '-e configuration_secure_version=$CONFIGURATION_SECURE_VERSION '
-                                              '-e cache_id=$CACHE_ID '
-                                              '-e ec2_region=$EC2_REGION '
-                                              '-e artifact_path=`/bin/pwd`/../../../$ARTIFACT_PATH '
-                                              '-e edx_app_theme_repo=$EDX_APP_THEME_REPO '
-                                              '-e edx_app_theme_version=$EDX_APP_THEME_VERSION '
-                                              '-e hipchat_token=$HIPCHAT_TOKEN '
-                                              '-e hipchat_room="$HIPCHAT_ROOM" '
-                                              '-e ami_wait=$AMI_WAIT '
-                                              '-e no_reboot=$NO_REBOOT '
-                                              'create_ami.yml'],
-                          working_dir="configuration/playbooks/continuous_delivery/"
-                          )
-                 )
+    job.add_task(ExecTask(['/bin/bash',
+                           '-c',
+                           'ansible-playbook '
+                           '-vvvv '
+                           '--module-path=../../playbooks/library '
+                           '-i "localhost," '
+                           '-c local '
+                           '-e @../../../target/launch_info.yml '
+                           '-e play=$PLAY '
+                           '-e deployment=$DEPLOYMENT '
+                           '-e edx_environment=$EDX_ENVIRONMENT '
+                           '-e cluster_repo=$CLUSTER_REPO '
+                           '-e cluster_version=$CLUSTER_VERSION '
+                           '-e configuration_repo=$CONFIGURATION_REPO '
+                           '-e configuration_version=$CONFIGURATION_VERSION '
+                           '-e configuration_secure_repo=$CONFIGURATION_SECURE_REPO '
+                           '-e configuration_secure_version=$CONFIGURATION_SECURE_VERSION '
+                           '-e cache_id=$CACHE_ID '
+                           '-e ec2_region=$EC2_REGION '
+                           '-e artifact_path=`/bin/pwd`/../../../$ARTIFACT_PATH '
+                           '-e edx_app_theme_repo=$EDX_APP_THEME_REPO '
+                           '-e edx_app_theme_version=$EDX_APP_THEME_VERSION '
+                           '-e hipchat_token=$HIPCHAT_TOKEN '
+                           '-e hipchat_room="$HIPCHAT_ROOM" '
+                           '-e ami_wait=$AMI_WAIT '
+                           '-e no_reboot=$NO_REBOOT '
+                           'create_ami.yml'],
+                          working_dir="configuration/playbooks/continuous_delivery/"))
 
     # Cleanup EC2
-    job.add_task(ExecTask(['/bin/bash', '-c', 'ansible-playbook '
-                                              '-vvvv '
-                                              '--module-path=../../configuration/playbooks/library '
-                                              '-i "localhost," '
-                                              '-c local '
-                                              '-e @../../../target/launch_info.yml '
-                                              '-e @../../../target/ami.yml '
-                                              '-e ec2_region=$EC2_REGION '
-                                              '-e hipchat_token=$HIPCHAT_TOKEN '
-                                              '-e hipchat_room="$HIPCHAT_ROOM" '
-                                              'cleanup.yml'],
-                          working_dir="configuration/playbooks/continuous_delivery/"
-                          )
-                 )
+    job.add_task(ExecTask(['/bin/bash',
+                           '-c',
+                           'ansible-playbook '
+                           '-vvvv '
+                           '--module-path=../../configuration/playbooks/library '
+                           '-i "localhost," '
+                           '-c local '
+                           '-e @../../../target/launch_info.yml '
+                           '-e @../../../target/ami.yml '
+                           '-e ec2_region=$EC2_REGION '
+                           '-e hipchat_token=$HIPCHAT_TOKEN '
+                           '-e hipchat_room="$HIPCHAT_ROOM" '
+                           'cleanup.yml'],
+                          working_dir="configuration/playbooks/continuous_delivery/"))
 
     configurator.save_updated_config(save_config_locally=save_config_locally, dry_run=dry_run)
 
--- reverse_engineer.py
+++ reverse_engineer.py
@@ -1,3 +1,10 @@
+"""
+This script provides a basic template for pipelines reversed engineered by gomatic.
+
+Simply reverse engineer a script:
+python -m reverse_engineer --ssl -s <server_address> -p <pipeline_name> --username <username> --password <password> > new_script.py
+
+"""
 import sys
 from gomatic import go_cd_configurator
 
@@ -11,9 +18,9 @@ def as_python(self, pipeline, with_save=True):
     head += "@click.command()\n"\
             "@click.option('--save-config', 'save_config_locally', envvar='SAVE_CONFIG', help='Save the pipeline configuration xml locally', required=False, default=False)\n"\
             "@click.option('--dry-run', envvar='DRY_RUN', help='do a dry run of  the pipeline installation, and save the pre/post xml configurations locally', required=False, default=False)\n"\
-            "@click.option('--variable_file', 'variable_files', multiple=True, help='path to yaml variable file with a dictionary of key/value pairs to be used as variables in the script', required=False)\n"\
-            "@click.option('-e', '--variable', 'cmd_line_vars', multiple=True, help='key/value of a variable used as a replacement in this script', required=False, type=(str, str), nargs=2)\n"\
-            "def install_pipeline(save_config_locally, dry_run, variable_files=[], cmd_line_vars={}):\n"
+            "@click.option('--variable_file', 'variable_files', multiple=True, help='path to yaml variable file with a dictionary of key/value pairs to be used as variables in the script', required=False, , default=[])\n"\
+            "@click.option('-e', '--variable', 'cmd_line_vars', multiple=True, help='key/value of a variable used as a replacement in this script', required=False, type=(str, str), nargs=2, , default={})\n"\
+            "def install_pipeline(save_config_locally, dry_run, variable_files, cmd_line_vars):\n"
 
     result = "config = utils.merge_files_and_dicts(variable_files, cmd_line_vars)\n\n"
     result += "configurator = " + str(self) + "\n"
